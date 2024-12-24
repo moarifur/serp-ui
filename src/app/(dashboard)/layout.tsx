b187b836975a6e78dde79e1e15f411e18e6e0d25
moarifur <moarifur@gmail.com>
2024-12-24 19:28:49 +0600

    Layout Seperation for Dashboard

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+const DashboardLayout = () => {
+    return (
+        <div className='h-screen flex'>
+          {/* Left */}
+          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-50">L</div>
+          {/* Right */}
+          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-100">R</div>
+        </div>
+    );
+};
 
-export default function DashboardLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
-  return <div>Dashboard{children}</div>
-}
+export default DashboardLayout;
