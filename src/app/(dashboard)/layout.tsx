ea1e93f07c5cff1700aa31ebfa4987f1694435f8
arif <moarifur@gmail.com>
2024-12-26 12:22:53 +0600

    Admin Dashboard UI design Completed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,10 +1,28 @@
-const DashboardLayout = () => {
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
+
+const DashboardLayout = ({ children }: Readonly<{ children: React.ReactNode; }>)  => {
     return (
-        <div className='h-screen flex'>
-          {/* Left */}
-          <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-amber-50">L</div>
-          {/* Right */}
-          <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-100">R</div>
+        <div className="h-screen flex">
+            {/* LEFT */}
+            <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+                <Link
+                    href="/"
+                    className="flex items-center justify-center lg:justify-start gap-2"
+                >
+                    <Image src="/logo.png" alt="logo" width={32} height={32}/>
+                    <span className="hidden lg:block font-bold">SchooLama</span>
+                </Link>
+                <Menu/>
+            </div>
+            {/* RIGHT */}
+            <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+                <Navbar/>
+                {children}
+            </div>
         </div>
     );
 };
